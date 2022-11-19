95a1329477802a202858afdf014a69129f7d16b5
iwatkot <iwatkot@gmail.com>
2022-11-19 10:41:08 +0300

    Changed some settings.

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,9 +19,10 @@ class AlienInvasion:
         """Game initialization and preparing game resources."""
         pygame.init()
         self.settings = Settings()
-        #self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN) #| FULLSCREEN
-        #self.settings.screen_width = self.screen.get_rect().width        #| OPTION
-        #self.settings.screen_height = self.screen.get_rect().height      #| ENABLE
+        # Enables fullscreen option.
+        #self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
